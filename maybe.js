c58de34f5c058776af7fef7293dba7c5ae97b18f
Chris Jones <chris.jones@twofour.co.uk>
2012-08-30 16:12:58 +0100

    first commit

---
+++ maybe.js
@@ -0,0 +1,50 @@
+if (typeof exports === 'object' && typeof define !== 'function') {
+  define = function (factory) {
+    module.exports = factory(require, exports, module);
+  };
+}
+
+define(function (require, exports, module) {
+
+  var Maybe = function (value) {
+    this.value = value;
+  };
+  Maybe.prototype = {
+    just : function (value) {
+      return new Maybe(value);
+    },
+
+    nothing: function() {
+      return new Maybe(null);
+    },
+
+    hasValue : function () {
+      return (this.value !== null && this.value !== undefined);
+    },
+
+    isJust : function () {
+      return this instanceof Maybe && this.hasValue();
+    },
+
+    isNothing : function () {
+      return this instanceof Maybe  && !this.hasValue();
+    },
+
+    failover : function (def, func) {
+      if(this instanceof Maybe && this.isJust()) {
+        if(func !== undefined && func instanceof Function) {
+          return func(this.value);
+        }
+        return this.value;
+      }
+      return def;
+    },
+
+    bind : function (binder) {
+      return this.isJust() ? new Maybe(binder) : this.nothing();
+    }
+  };
+
+  return new Maybe();
+
+});
---
+++ tests/specs.js
@@ -0,0 +1,64 @@
+var Maybe = require('../maybe.js');
+var expect = require('expect.js');
+
+describe('Given a maybe with just a value', function () {
+
+  var the_value = "the value";
+  var the_maybe = Maybe.just(the_value);
+
+  describe('when checking its state', function () {
+    it('should have a value', function () {
+      expect(the_maybe.hasValue()).to.be(true);
+    });
+
+    it('should provide the value', function (){
+      expect(the_maybe.value).to.be(the_value);
+    });
+  });
+
+  describe('when failing over', function () {
+    it('should provide the value', function () {
+      expect(the_maybe.failover("not the value")).to.be(the_value);
+    });
+  });
+
+  describe('when binding', function () {
+    it('should provide a maybe containing the bound value', function () {
+      var the_bound_value = "bound value";
+      var result = the_maybe.bind(the_bound_value);
+      expect(result.value).to.be(the_bound_value);
+    });
+  });
+
+});
+
+describe('Given a maybe with nothing', function () {
+
+  var the_maybe = Maybe.nothing();
+
+  describe('when checking its state', function () {
+    it('should not have a value', function () {
+      expect(the_maybe.hasValue()).to.not.be(true);
+    });
+
+    it('should provide the value', function (){
+      expect(the_maybe.value).to.not.be.ok();
+    });
+  });
+
+  describe('when failing over', function () {
+    it('should provide the value', function () {
+      var not_the_value ="not the value";
+      expect(the_maybe.failover(not_the_value)).to.be(not_the_value);
+    });
+  });
+
+  describe('when binding', function () {
+    it('should provide a maybe containing nothing', function () {
+      var the_bound_value = "bound value";
+      var result = the_maybe.bind(the_bound_value);
+      expect(result.isNothing()).to.be(true);
+    });
+  });
+
+});
